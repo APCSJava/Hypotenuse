9730a86958b43b1325b9537d2e9d43374a04ed5f
kentcollins <kent.r.collins@gmail.com>
2016-03-17 11:04:04 +0000

    Added mystery.

---
+++ Hypotenuse.java
@@ -0,0 +1,19 @@
+
+public class Hypotenuse {
+
+	public static void main(String[] args) {
+		double a = 3.0;
+		double b = 4.0;
+		double hyp = getHypotenuse(a, b);
+		System.out.println("The hyptotenuse is "+hyp);
+	}
+	
+	public static double getHypotenuse(double sideA, double sideB) {
+		double a2 = sideA*sideA;
+		double b2 = sideB*sideB;
+		double sum = a2 + b2;
+		double h = Math.sqrt(sum);
+		return h;
+	}
+
+}
